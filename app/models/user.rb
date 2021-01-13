0488fd89b347a75122f78b46015b0bde9b107f07
seki774 <75604754+seki774@users.noreply.github.com>
2021-01-13 18:39:28 +0900

    正規表現までの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        # validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+        # validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+        # validates :last_name_hurigana, format: { with: /\A[/^([ァ-ン]|ー)+$/]+\z/}
+        # validates :first_name_hurigana, format: { with: /\A[/^([ァ-ン]|ー)+$/]+\z/}
+
 end
