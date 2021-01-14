71ed625d57e5614176382ef6ca097ddd371dec23
seki774 <75604754+seki774@users.noreply.github.com>
2021-01-14 19:06:31 +0900

    ルボコップ実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
-         validates :nickname, presence: true 
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_hurigana, presence: true
-         validates :last_name_hurigana, presence: true
-         #validates :last_name, format:  { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
-         #validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
-         #validates :last_name_hurigana, format:  { with: /\A[/^([ァ-ン]|ー)+$/]+\z/}
-         #validates :first_name_hurigana, format: { with: /\A[/^([ァ-ン]|ー)+$/]+\z/}
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-          validates :first_name
-          validates :last_name
-         end
-         with_options presence: true, format: { with: /\A[ァ-ヶー-]+\z/, message: '全角カタカナを使用してください' } do
-          validates :first_name_hurigana
-          validates :last_name_hurigana
-         end
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_hurigana, presence: true
+  validates :last_name_hurigana, presence: true
+  # validates :last_name, format:  { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+  # validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+  # validates :last_name_hurigana, format:  { with: /\A[/^([ァ-ン]|ー)+$/]+\z/}
+  # validates :first_name_hurigana, format: { with: /\A[/^([ァ-ン]|ー)+$/]+\z/}
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー-]+\z/, message: '全角カタカナを使用してください' } do
+    validates :first_name_hurigana
+    validates :last_name_hurigana
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"test"}
-    email                 {"test@example"}
-    password              {"000000"}
-    password_confirmation {password}
-    first_name            {"あ"}
-    last_name             {"あ"}
-    first_name_hurigana            {"ア"}
-    last_name_hurigana            {"ア"}
-    birthday              {'1988/04/07'}
+    nickname              { 'test' }
+    email                 { 'test@example' }
+    password              { '000000' }
+    password_confirmation { password }
+    first_name            { 'あ' }
+    last_name             { 'あ' }
+    first_name_hurigana { 'ア' }
+    last_name_hurigana { 'ア' }
+    birthday { '1988/04/07' }
   end
 end
