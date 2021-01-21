5a67ddf35749d2a7ea7ba471d8b2c9fbc9863920
seki774 <75604754+seki774@users.noreply.github.com>
2021-01-21 14:36:07 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
   def index
     @goods = Good.all.order("created_at DESC")
   end
@@ -30,15 +31,23 @@ class ItemsController < ApplicationController
     @good = Good.find(params[:id])    
     @good.update(good_params)
    if @good.save 
-    redirect_to root_path
+    redirect_to root_path 
     else
       render :new
     end
   end
+  
   private
 
   def good_params
     params.require(:good).permit(:goods_name, :goods_price, :goods_text, :category_id, :good_state_id, :delivery_fee_id,
                                  :ship_area_id, :day_id, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
+
