7c99a4c958749dc1ed1be4cf2949aa97cfb80c53
seki774 <75604754+seki774@users.noreply.github.com>
2021-01-21 15:46:19 +0900

    editを削除　修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
   before_action :params_id, only: [:show, :edit, :update]
   def index
